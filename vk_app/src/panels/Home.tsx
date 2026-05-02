64b3a3638733744a68f1ef36479475ca06b65f3f
anton dvinyaninov <antondvinyaninov@icloud.com>
2026-05-02 13:11:20 +0400

    Remove VK link button from Home feed

--- vk_app/src/panels/Home.tsx
+++ vk_app/src/panels/Home.tsx
@@ -185,17 +185,6 @@ export const Home: FC<HomeProps> = ({ id }) => {
                   >
                     Подробнее
                   </Button>
-                  
-                  {post.vk_post_id && post.group?.vk_group_id && (
-                    <Button 
-                      size="s" 
-                      mode="secondary" 
-                      stretched
-                      onClick={() => window.open(`https://vk.com/wall-${post.group.vk_group_id}_${post.vk_post_id}`, '_blank')}
-                    >
-                      ВКонтакте
-                    </Button>
-                  )}
 
                   <Button 
                     size="s" 
